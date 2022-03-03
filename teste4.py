844876c4f11dcdd71f262caab0043dc826f0f155
Tashaigg <99045602+Tashaigg@users.noreply.github.com>
2022-03-03 10:45:26 -0300

    full digit

--- teste4.py
+++ teste4.py
@@ -5,5 +5,18 @@
 
 
 linha = ("2 * 3 =")
+n1=""
+n2=""
 num = int(linha[0])*int(linha[4])
-print(num)
+print(len(linha))
+
+for cha in range(len(linha)):
+    print(cha)
+    if linha[cha].isdigit():
+        if n1:
+            n2=linha[cha]
+        else:
+            n1=linha[cha]
+
+print(f'n1= {n1} n2= {n2}')
+print(int(n1)*int(n2))
